00adca93c0de4a79b74a958742b8f3b0bed08c8d
skalogerakis <skalogerakis97@gmail.com>
2020-02-16 01:42:32 +0200

    Added state to remember add and count(flatMap)

--- ECE622/src/main/java/tuc/RandomSampling.java
+++ ECE622/src/main/java/tuc/RandomSampling.java
@@ -8,6 +8,7 @@ import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -22,29 +23,22 @@ public class RandomSampling {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         //DataStream<Tuple2<String, Float>> csvInput = env.readTextFile("/home/skalogerakis/TUC_Projects/TUC_Advanced_Database_Systems/MyDocs/openaq_Bosnia.csv")
-//        DataStream<Tuple2<String, Float>> csvInput = env.readTextFile("/home/skalogerakis/Downloads/openaq.csv")
-//                .map(new MapFunction<String, Tuple2<String, Float>>() {
-//                    @Override
-//                    public Tuple2<String, Float> map(String value) throws Exception {
-//                        String[] words = value.split(",");
-//                        Tuple2<String, Float> t1 = new Tuple2<>(words[0], Float.parseFloat(words[6]));
-//                        return t1;
-//                    }
-//                });
 
-        DataStream<Tuple2<String, Float>> csvInput = env.readTextFile("/home/skalogerakis/Downloads/openaq.csv")
+        //This is for data input purposes only. TODO replace that with Kafka implementation
+        //Current way data are transformed Tuple2<String, Float>
+        DataStream<Tuple2<String, Float>> input = env.readTextFile("/home/skalogerakis/Downloads/openaq.csv")
                 .flatMap(new FlatMapFunction<String, Tuple2<String, Float>>() {
             @Override
             public void flatMap(String value, Collector<Tuple2<String, Float>> out)
                     throws Exception {
                 String[] words = value.split(",");
-                Tuple2<String, Float> t1 = new Tuple2<>(words[0], Float.parseFloat(words[6]));
-                out.collect(t1);
+                Tuple2<String, Float> temp1 = new Tuple2<>(words[0], Float.parseFloat(words[6]));
+                out.collect(temp1);
             }
         });
 
-        DataStream<Tuple2<String, Float>> sum = csvInput.keyBy(0).sum(1);
 
+        DataStream<Tuple3<String, Float,Float>> sum = input.keyBy(0).flatMap(new CalcImplementation());
         sum.print();
 
         //execute program to see action
@@ -52,44 +46,48 @@ public class RandomSampling {
 
     }
 
-//    public class CountWindowAverage extends RichFlatMapFunction<Tuple2<String, Long>, Tuple2<String, Long>> {
-//
-//        /**
-//         * The ValueState handle. The first field is the count, the second field a running sum.
-//         */
-//        private transient ValueState<Tuple2<Long, Long>> sum;
-//
-//        @Override
-//        public void flatMap(Tuple2<Long, Long> input, Collector<Tuple2<Long, Long>> out) throws Exception {
-//
-//            // access the state value
-//            Tuple2<Long, Long> currentSum = sum.value();
-//
-//            // update the count
-//            currentSum.f0 += 1;
-//
-//            // add the second field of the input value
-//            currentSum.f1 += input.f1;
-//
-//            // update the state
-//            sum.update(currentSum);
-//
-//            // if the count reaches 2, emit the average and clear the state
-//            if (currentSum.f0 >= 2) {
-//                out.collect(new Tuple2<>(input.f0, currentSum.f1 / currentSum.f0));
-//                sum.clear();
-//            }
-//        }
-//
-//        @Override
-//        public void open(Configuration config) {
-//            ValueStateDescriptor<Tuple2<Long, Long>> descriptor =
-//                    new ValueStateDescriptor<>(
-//                            "average", // the state name
-//                            TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {}), // type information
-//                            Tuple2.of(0L, 0L)); // default value of the state, if nothing was set
-//            sum = getRuntimeContext().getState(descriptor);
-//        }
-//    }
+    //TODO add mean and variance
+    public static class CalcImplementation extends RichFlatMapFunction<Tuple2<String, Float>, Tuple3<String, Float, Float>> {
+
+        /**
+         * The ValueState handle. The first field is the key, the second field a running sum, the third a count of all elements.
+         */
+        private transient ValueState<Tuple3<String, Float,Float>> sum;
+
+        @Override
+        public void flatMap(Tuple2<String, Float> input, Collector<Tuple3<String, Float, Float>> out) throws Exception {
+
+            // access the state value
+            Tuple3<String, Float,Float> currentSum = sum.value();
+
+            if(currentSum == null){
+                currentSum = Tuple3.of(input.f0,0.0F,0.0F);
+            }
+            // update the count
+            currentSum.f2++;
+            // add the second field of the input value
+            currentSum.f1 += input.f1;
+
+            // update the state
+            sum.update(currentSum);
+
+            out.collect(new Tuple3<>(input.f0, currentSum.f1,currentSum.f2));
+        }
+
+        //Initialization inside ValueStateDescriptor is Deprecated. Must check null case and initialize in flatMap function
+        @Override
+        public void open(Configuration config) {
+
+            //StateDescriptor holds name and characteristics of state
+            ValueStateDescriptor<Tuple3<String, Float,Float>> descriptor = new ValueStateDescriptor<>(
+                            "sum", // the state name
+                            TypeInformation.of(new TypeHint<Tuple3<String, Float,Float>>() {})); // type information
+                            //Tuple3.of("",0.0F,0.0F)); // default value of the state, if nothing was set
+            sum = getRuntimeContext().getState(descriptor);     //Access state using getRuntimeContext()
+        }
+
+    }
+
+
 
 }
