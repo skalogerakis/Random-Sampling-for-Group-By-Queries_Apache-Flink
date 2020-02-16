8d23ae9b8537aeff78b4250f8832034be032851d
skalogerakis <skalogerakis97@gmail.com>
2020-02-16 20:30:38 +0200

    Added mean, variance. First pass completed

--- ECE622/src/main/java/tuc/RandomSampling.java
+++ ECE622/src/main/java/tuc/RandomSampling.java
@@ -9,10 +9,9 @@ import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.checkpoint.MasterTriggerRestoreHook;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -36,25 +35,29 @@ public class RandomSampling {
 
         //This is for data input purposes only. TODO replace that with Kafka implementation
         //Current way data are transformed Tuple2<String, Float>
-        DataStream<Tuple2<String, Float>> input = env.readTextFile("/home/skalogerakis/Downloads/openaq.csv")
-                .flatMap(new FlatMapFunction<String, Tuple2<String, Float>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<String, Float>> out)
-                    throws Exception {
-                String[] words = value.split(",");
-                Tuple2<String, Float> temp1 = new Tuple2<>(words[0], Float.parseFloat(words[6]));
-                out.collect(temp1);
-            }
-        });
+        DataStream<Tuple2<String, Double>> input = env.readTextFile("/home/skalogerakis/Downloads/openaq.csv")
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Double>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Double>> out)
+                            throws Exception {
+                        String[] words = value.split(",");
+                        Tuple2<String, Double> temp1 = new Tuple2<>(words[0], Double.parseDouble(words[6]));
+                        out.collect(temp1);
+                    }
+                });
 
         //FlatMap implementation
         //DataStream<Tuple3<String, Float,Float>> sum = input.keyBy(0).flatMap(new CalcImplementation());
 
         //KeyedProcessFunction implementation
-        DataStream<Tuple3<String, Float,Float>> sum = input
+        //                  key   sum   count   mean   var
+        DataStream<Tuple5<String,Double,Double,Double,Double>> sum = input
                 .keyBy(0)
                 .process(new CalcImplementation());
+
         sum.print();
+
+
 
         //Using side stream
 //        SingleOutputStreamOperator<Tuple3<String, Float,Float>> sum = input
@@ -121,8 +124,10 @@ public class RandomSampling {
     public class ImplementationFields {
 
         public String key;
-        public float sum;
-        public float count;
+        public double sum;
+        public double count;
+        public double var;
+        public double mean;
         //public long lastModified;
     }
 
@@ -133,33 +138,47 @@ public class RandomSampling {
      */
     //TODO also if there is time implement fault tolerance https://ci.apache.org/projects/flink/flink-docs-release-1.10/dev/stream/operators/process_function.html
     //TODO add mean and variance
-    public static class CalcImplementation extends KeyedProcessFunction<Tuple, Tuple2<String, Float>, Tuple3<String, Float, Float>> {
+    public static class CalcImplementation extends KeyedProcessFunction<Tuple, Tuple2<String, Double>, Tuple5<String, Double, Double,Double,Double>> {
 
         /**
          * The ValueState handle. The first field is the key, the second field a running sum, the third a count of all elements.
          */
-        private transient ValueState<Tuple3<String, Float,Float>> sum;
+        private transient ValueState<Tuple7<String, Double,Double,Double,Double,Double,Double>> sum;
         //private transient ValueState<ImplementationFields> sum;
 
         @Override
-        public void processElement(Tuple2<String, Float> input,Context ctx, Collector<Tuple3<String, Float, Float>> out) throws Exception {
+        public void processElement(Tuple2<String, Double> input,Context ctx, Collector<Tuple5<String, Double, Double,Double,Double>> out) throws Exception {
 
             // access the state value
-            Tuple3<String, Float,Float> currentSum = sum.value();
+            Tuple7<String, Double,Double,Double,Double,Double,Double> currentSum = sum.value();
             //ImplementationFields currentSum = sum.value();
 
             if(currentSum == null){
-                currentSum = Tuple3.of(input.f0,0.0F,0.0F);
+                currentSum = Tuple7.of(input.f0,0.0D,0.0D,0.0D,0.0D,0.0D,0.0D);
             }
             // update the count
             currentSum.f2++;
             // add the second field of the input value
             currentSum.f1 += input.f1;
+            //sum(xi^2)
+            currentSum.f4 += Math.pow(input.f1,2);
+
+            //mean(x)
+            currentSum.f3 =  currentSum.f1/currentSum.f2;
+
+            //mean(x^2)
+            currentSum.f5 =  currentSum.f4/currentSum.f2;
+
+            // CALC σ (standard deviation)
+            currentSum.f6=Math.sqrt( (currentSum.f5-Math.pow(currentSum.f3,2)) );
+
+            //System.out.println(" sum="+currentSum.f1+" count="+currentSum.f2+" mean="+currentSum.f3 +" x2="+currentSum.f4 +" mean2="+currentSum.f5+" σ ="+currentSum.f6);
+
 
             // update the state
             sum.update(currentSum);
 
-            out.collect(new Tuple3<>(input.f0, currentSum.f1,currentSum.f2));
+            out.collect(new Tuple5<>(input.f0, currentSum.f1,currentSum.f2,currentSum.f3,currentSum.f6));
             //Added for side output implementation
             //ctx.output(outputTag, "sideout-" + String.valueOf(input.f0));
         }
@@ -169,9 +188,9 @@ public class RandomSampling {
         public void open(Configuration config) {
 
             //StateDescriptor holds name and characteristics of state
-            ValueStateDescriptor<Tuple3<String, Float,Float>> descriptor = new ValueStateDescriptor<>(
+            ValueStateDescriptor<Tuple7<String, Double,Double,Double,Double,Double,Double>> descriptor = new ValueStateDescriptor<>(
                     "sum", // the state name
-                    TypeInformation.of(new TypeHint<Tuple3<String, Float,Float>>() {})); // type information
+                    TypeInformation.of(new TypeHint<Tuple7<String, Double,Double,Double,Double,Double,Double>>() {})); // type information
             //Tuple3.of("",0.0F,0.0F)); // default value of the state, if nothing was set
             sum = getRuntimeContext().getState(descriptor);     //Access state using getRuntimeContext()
 
